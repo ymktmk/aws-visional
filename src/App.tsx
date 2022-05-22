e006bbad31ab0e43686026c4a74d96dd2bf3a5fa
ymktmk <73768462+ymktmk@users.noreply.github.com>
2022-05-22 11:30:26 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { AWS, InvizAWS, EC2, Lambda, Region, SecurityGroup, AutoScalingGroup } from '@rediagram/aws';
+
+export const App: React.FC = () => {
+  return <InvizAWS></InvizAWS>
+}
